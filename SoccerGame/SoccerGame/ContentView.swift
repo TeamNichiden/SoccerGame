650e5d2f92049ead5d45c88a616f53db04833945
ラワンアウンピョウ <hlumaungphyo@gmail.com>
2024-07-20 14:54:48 +0900

    [作成]プロジェクト

---
+++ SoccerGame/SoccerGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SoccerGame
+//
+//  Created by Hlwan Aung Phyo on 2024/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SoccerGame/SoccerGame/SoccerGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoccerGameApp.swift
+//  SoccerGame
+//
+//  Created by Hlwan Aung Phyo on 2024/07/20.
+//
+
+import SwiftUI
+
+@main
+struct SoccerGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
