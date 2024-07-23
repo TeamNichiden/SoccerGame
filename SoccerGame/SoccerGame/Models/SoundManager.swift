bf413dd2d79abc6adecdc209601485a32b9c0a3a
ラワンアウンピョウ <53109660+hlum@users.noreply.github.com>
2024-07-23 12:21:41 +0900

    View/start screen (#9)
    * [追加]StartボタンとHighscoreボタン追加
    
    * [追加]背景、キャラ選択ボタン
    [変更]Toolbar(Highscore button) -> 下に移動
    
    * [追加]ボール
    [整理]解読やすさ向上
    
    * [追加]ボールにアニメーション
    [追加]ボールまわりリング入れてアニメーションつけ
    [変更]背景の写真削除ー＞真っ白
    
    * [追加]ボールにアニメーション
    
    * [追加]ボールまわりリング入れてアニメーションつけ
    [変更]背景の写真削除ー＞真っ白
    
    * [変更]ボールのアニメーション[linear -> bouncy]
    
    * [追加]SoundPlayer Class実装
    
    * [追加]EnvironmentにSoundManagerを入れアプリ全体に配布
    [作成]設定のVIEWを作成し、BC音声を調整できるボタン

---
+++ SoccerGame/SoccerGame/Models/SoundManager.swift
@@ -0,0 +1,39 @@
+//
+//  SoundManager.swift
+//  SoccerGame
+//
+//  Created by Hlwan Aung Phyo on 2024/07/22.
+//
+
+import Foundation
+import UIKit
+import AVFoundation
+
+class SoundManager: ObservableObject{
+    
+    private var soundPlayer :AVAudioPlayer?
+    @Published var volume : Float = 1.0
+    
+    
+    func playSound(FileName:String,FileType:String,loop:Bool){
+        guard let url = Bundle.main.url(forResource: FileName, withExtension: FileType) else{
+        print("File not found")
+            return
+        }
+        do{
+            soundPlayer = try AVAudioPlayer(contentsOf: url)
+            soundPlayer?.numberOfLoops = loop ? -1 : 0
+            soundPlayer?.volume = volume
+            soundPlayer?.play()
+        }catch let error{
+            print("SoundPlayer can't be created\(error.localizedDescription)")
+        }
+        
+    }
+    
+    func stopSound(){
+        soundPlayer?.stop()
+    }
+    
+    
+}
