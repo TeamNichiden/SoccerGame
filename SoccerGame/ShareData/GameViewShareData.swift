14a05c02103942a0107999bf006688e8ed06672b
Saikaku <grnmnsta@gmail.com>
2024-07-26 09:44:10 +0900

    Play画面の作成および追加

---
+++ SoccerGame/ShareData/GameViewShareData.swift
@@ -0,0 +1,18 @@
+//
+//  GameViewShareData.swift
+//  SoccerGame
+//
+//  Created by cmStudent on 2024/07/26.
+//
+
+import SwiftUI
+
+struct GameViewShareData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameViewShareData()
+}
---
+++ SoccerGame/SoccerGame/Views/PowerView.swift
@@ -0,0 +1,18 @@
+//
+//  PowerView.swift
+//  SoccerGame
+//
+//  Created by cmStudent on 2024/07/26.
+//
+
+import SwiftUI
+
+struct PowerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PowerView()
+}
